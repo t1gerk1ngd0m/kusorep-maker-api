b0b3b4b3e161104a053e7853e784e9b7398243a9
Taiga Miwa <taiga.miwa888@gmail.com>
2021-04-04 22:47:05 +0900

    delete comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-
-  # if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  # end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 end
